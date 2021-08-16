217b49f676e7c0c55b37038d04ad8c168e630a35
jose manuel <jose.ibaceta@usach.cl>
2021-08-16 15:03:26 -0400

    Avance 2 (normal completa)

--- mycdf.m
+++ mycdf.m
@@ -1,16 +1,35 @@
 function [x,F] = mycdf(distname,a,b,mean,sigma)
-f = 0;
-if distame == "Normal" || distame == "normal" || distame == "N" || distname == "n"
-    distname = "Normal";
-    f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
-elseif  (distame == "Uniform" || distame == "uniform" || distame == "U" || distname == "u" || distname == "uniforme" || distname == "Uniforme")
-    distname = "Uniform";
-    f = @(x) 1/(b - a);
-else
-    distname = "Normal";
-    f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
-x = [];
-F = [];
+    f = 0;
+    x = zeros(1,1000);
+    F = zeros(1,1000);
+    if distname == "Normal" || distname == "normal" || distname == "N" || distname == "n"
+        distname = "Normal";
+        f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
+        for i=1:1000
+            x(i)= random('Normal',a,b);
+            x=sort(x);
+            F(i)=integral(f, - inf , x(i));
+        end
+    elseif  (distname == "Uniform" || distname == "uniform" || distname == "U" || distname == "u" || distname == "uniforme" || distname == "Uniforme")
+        distname = "Uniform";
+        f = @(x) 1/(b - a);
+        for i=1:1000
+            x(i)= random('uniform',a,b);
+            x=sort(x);
+            F(i)=integral(f, a, x(i));
+        end
+    else
+        distname = "Normal";
+        f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
+        for i=1:1000
+            x(i)= random('Normal',a,b);
+            x=sort(x);
+            F(i)=integral(f, - inf , x(i));
+        end
+end
+    
+
+
 %f_derivada = polyder(funcion);
 
 %f_x0 = polyval(funcion, x0);
