e95168def7862e8716e6b8feed0473d60fa5acab
Victorh <victor.huanqui@usach.cl>
2021-08-17 16:13:49 -0400

    Se agregaron comentarios y pequeñas modificaciones

--- mycdf.m
+++ mycdf.m
@@ -4,39 +4,43 @@ Integrantes:
     José Ibaceta
     Victor Huanqui
 %}
-function [x,F] = mycdf(distname,a,b,mean,sigma)
-    f = 0;
+% Función simulador de la CDF
+function [x,F] = mycdf(distname, a, b, mean, sigma)
+    % se definen los vectores 
     x = zeros(1,1000);
     F = zeros(1,1000);
+    
     if distname == "Normal" || distname == "normal" || distname == "N" || distname == "n"
-        distname = "Normal";
+        % Se obtienen valores aleatorios para la distribución normal
+        for i = 1 : 1000
+            x(i) = random('Normal', a, b); 
+        end        
+        x = sort(x);
+        
+        % se define la función densidad (PDF)
+        f = @(x)(exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
+        
+        % se obtiene la CDF
+        for i = 1 : 1000
+            F(i) = integral(f, -inf , x(i));
+        end
+    elseif distname == "Uniform" || distname == "uniform" || distname == "U" || distname == "u" || distname == "uniforme" || distname == "Uniforme"
+        % Se obtienen valores aleatorios para la distribución uniforme
         for i=1:1000
-            x(i)= random('Normal',a,b);
+            x(i)= random('uniform', a, b);
         end
         x=sort(x);
-        f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
-        for i=1:1000
-            F(i)=integral(f, - inf , x(i));
-        end
-    elseif  (distname == "Uniform" || distname == "uniform" || distname == "U" || distname == "u" || distname == "uniforme" || distname == "Uniforme")
-        distname = "Uniform";
-        f = @(x) 1/(b - a);
-        for i=1:1000
-            x(i)= random('uniform',a,b);
-        end
-        x=sort(x);
-        for i=1:1000
-            F(i)=(x(i)-a)/(b-a);
+        
+        % se obtiene la CDF
+        for i = 1 : 1000
+            F(i) = (x(i)-a)/(b-a);
         end
     else
-        distname = "Normal";
-        for i=1:1000
-            x(i)= random('Normal',a,b);
-        end
-        x=sort(x);
-        f = @(x) (exp((-((x - mean)/sigma).^2)/2))/(sigma*(2*pi)^(1/2));
-        for i=1:1000
-            F(i)=integral(f, - inf , x(i));
-        end
+        disp("Incorrect distname")
+    end
+    % Graficando la CDF
+    figure(1);
+    plot(x, F);
+    title ('Gráfico F(x)');
 end
 
--- sumuniforme.m
+++ sumuniforme.m
@@ -4,17 +4,29 @@ Integrantes:
     José Ibaceta
     Victor Huanqui
 %}
-function [f,c,d] = sumuniforme(n, a, b)
-    f= zeros(1,1000);
-    vector=zeros(1,1000);
-    f=f+1/(b-a);
-    vector=vector+1/(b-a);
-    for j=1:(n-1)
-        f=conv(f,vector,'full');
+% Función suma uniforme
+function [f, c, d] = sumuniforme(n, a, b)
+    % se definen los vectores
+    f = zeros(1, 1000);
+    vector = zeros(1, 1000);
+    
+    % se crean las distribuciones uniformes
+    f = f + 1/(b-a);
+    vector = vector + 1/(b-a);
+    
+    % se convolucionan las distribuciones
+    for j = 1 : (n-1)
+        f = conv(f, vector, 'full');
     end
-    c=0;
-    d=0;
-    if (n==2 || n==8 || n==16 || n==32)
-       plot(f); 
+    
+    % definiendo rango de la convolución
+    c = min(f);
+    d = max(f);
+    
+    if ( n == 2 || n == 8 || n == 16 || n == 32)
+        % mostrando gráfico de aproximación 
+        figure(1);
+        plot(f);
+        title(['Aproximación para N = ' num2str(n)]);
     end
 end
